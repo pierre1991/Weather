70d039bf633f6fb2135ff36b9327da81fba86bc8
Pierre Toroyan <p.toroyan@gmail.com>
2016-10-26 11:07:25 -0700

    cleanup

--- Weather/AppDelegate.swift
+++ Weather/AppDelegate.swift
@@ -14,10 +14,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
 
     var window: UIWindow?
 
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-		
-        AppearanceController.initializeAppearance()
-        
+		AppearanceController.initializeAppearance()
         
         return true
     }
