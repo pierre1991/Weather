d4b4ffc2c5ed431c85de1c8459abe4d1aff689bf
Pierre Toroyan <p.toroyan@gmail.com>
2016-10-31 16:02:35 -0700

    SettingsLocationView CocoaTochFile

---
+++ Weather/SettingsLocationView.swift
@@ -0,0 +1,14 @@
+//
+//  SettingsLocationView.swift
+//  Weather
+//
+//  Created by Pierre on 10/31/16.
+//  Copyright © 2016 Pierre. All rights reserved.
+//
+
+import UIKit
+
+class SettingsLocationView: UIView {
+
+
+}
